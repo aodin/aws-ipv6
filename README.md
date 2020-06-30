eeb5bf3a7a744bae0ae5a5eb0f71a82acc889594
Aaron O. Ellis <aaronoellis@gmail.com>
2020-06-29 22:04:33 -0600

    Updated for Terraform 0.12

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 AWS IPv6
 ====
 
-Terraform config for an IPv6 capable EC2 instance on AWS.
+[Terraform 0.12](https://releases.hashicorp.com/terraform/0.12.28/) config for an IPv6 capable EC2 instance on AWS.
 
 [Official AWS documentation on IPv6 EC2 instances](https://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/vpc-subnets-commands-example-ipv6.html)
 
@@ -9,15 +9,17 @@ Terraform config for an IPv6 capable EC2 instance on AWS.
 
 1. Run `terraform init`
 
-2. A new key pair was created on the AWS console with the name `ipv6_key`.
+2. Create a key pair on the AWS console named `ipv6_key` or replace the `key_name` variable with a different key pair.
 
 3. Run `terraform plan -out=plan.tmp`
 
 4. Run `terraform apply "plan.tmp"`
+
+An nginx webserver will be installed and started by the commands in `userdata.tpl`. The server should display a welcome message on port `80`. You can connect directly to the server via its IPv6 address. Remember to wrap the address in brackets when using `curl` or a web browser, e.g. `http://[2600:1f14:e71:9600:e964:47df:2470:e710]`.
 
 5. If you'd like to destroy the created infrastructure, run `terraform destroy`
 
 
 Happy hacking!
 
-aodin, 2018
+aodin, 2018-2020
--- ipv6.tf
+++ ipv6.tf
@@ -1,40 +1,35 @@
 provider "aws" {
-  region  = "${var.aws_region}"
-  profile = "${var.aws_profile}"
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
 variable "aws_region" {
-  type    = "string"
   default = "us-west-2"
 }
 
 variable "aws_profile" {
-  type    = "string"
   default = ""
 }
 
 variable "key_name" {
-  type    = "string"
   default = "ipv6_key"
 }
 
 # Ubuntu AMIs from https://cloud-images.ubuntu.com/locator/ec2/
 variable "instance_ami" {
-  type    = "string"
-  default = "ami-22741f5a" # Ubuntu 18.04 HVM/EBS in us-west-2
+  default = "ami-043505d1b57b5d3e3" # Ubuntu 20.04 HVM/EBS in us-west-2
 }
 
 variable "instance_size" {
-  type    = "string"
-  default = "t2.nano"
+  default = "t3a.nano"
 }
 
 data "aws_availability_zones" "available" {}
 
 data "template_file" "userdata" {
-  template = "${file("userdata.tpl")}"
+  template = file("userdata.tpl")
 
-  vars {}
+  vars = {}
 }
 
 # AWS will assign the VPC an IPv6 CIDR with a prefix length of /56
@@ -44,23 +39,23 @@ resource "aws_vpc" "ipv6_vpc" {
   enable_dns_hostnames             = true
   assign_generated_ipv6_cidr_block = true
 
-  tags {
+  tags = {
     Name = "ipv6_vpc"
   }
 }
 
 # The subnet IPv6 CIDR must be in the VPC CIDR and have a prefix length of /64
 resource "aws_subnet" "public_ipv6" {
-  vpc_id          = "${aws_vpc.ipv6_vpc.id}"
-  cidr_block      = "${cidrsubnet(aws_vpc.ipv6_vpc.cidr_block, 4, 0)}"
-  ipv6_cidr_block = "${cidrsubnet(aws_vpc.ipv6_vpc.ipv6_cidr_block, 8, 0)}"
+  vpc_id          = aws_vpc.ipv6_vpc.id
+  cidr_block      = cidrsubnet(aws_vpc.ipv6_vpc.cidr_block, 4, 0)
+  ipv6_cidr_block = cidrsubnet(aws_vpc.ipv6_vpc.ipv6_cidr_block, 8, 0)
 
   map_public_ip_on_launch         = true
   assign_ipv6_address_on_creation = true
 
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone = data.aws_availability_zones.available.names[0]
 
-  tags {
+  tags = {
     Name = "public_ipv6"
   }
 }
@@ -69,13 +64,9 @@ resource "aws_subnet" "public_ipv6" {
 resource "aws_security_group" "ipv6_security" {
   name        = "ipv6_security"
   description = "IPv6 Security"
-  vpc_id      = "${aws_vpc.ipv6_vpc.id}"
+  vpc_id      = aws_vpc.ipv6_vpc.id
 
   revoke_rules_on_delete = true
-
-  tags {
-    Name = "ipv6_security"
-  }
 
   # Allow connections from any IPv4 or IPv6 address; this is not secure
   ingress {
@@ -93,53 +84,57 @@ resource "aws_security_group" "ipv6_security" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  tags = {
+    Name = "ipv6_security"
+  }
 }
 
 resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.ipv6_vpc.id}"
+  vpc_id = aws_vpc.ipv6_vpc.id
 
-  tags {
+  tags = {
     Name = "ipv6_gateway"
   }
 }
 
 resource "aws_route_table" "ipv6_route_table" {
-  vpc_id = "${aws_vpc.ipv6_vpc.id}"
+  vpc_id = aws_vpc.ipv6_vpc.id
 
   # Route all IPv6 traffic to the Internet gateway
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id      = "${aws_internet_gateway.default.id}"
+    gateway_id      = aws_internet_gateway.default.id
   }
 
   # Route all IPv4 traffic to the Internet gateway
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.default.id}"
+    gateway_id = aws_internet_gateway.default.id
   }
 
-  tags {
+  tags = {
     Name = "ipv6_subnet"
   }
 }
 
 # The route table must be associated with the subnet
 resource "aws_route_table_association" "public_ipv6" {
-  subnet_id      = "${aws_subnet.public_ipv6.id}"
-  route_table_id = "${aws_route_table.ipv6_route_table.id}"
+  subnet_id      = aws_subnet.public_ipv6.id
+  route_table_id = aws_route_table.ipv6_route_table.id
 }
 
 resource "aws_instance" "ipv6_instance" {
-  ami           = "${var.instance_ami}"
-  instance_type = "${var.instance_size}"
-  key_name      = "${var.key_name}"
-  subnet_id     = "${aws_subnet.public_ipv6.id}"
+  ami           = var.instance_ami
+  instance_type = var.instance_size
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.public_ipv6.id
 
-  vpc_security_group_ids      = ["${aws_security_group.ipv6_security.id}"]
+  vpc_security_group_ids      = [aws_security_group.ipv6_security.id]
   associate_public_ip_address = true
-  user_data                   = "${data.template_file.userdata.rendered}"
+  user_data                   = data.template_file.userdata.rendered
 
-  tags {
+  tags = {
     Name = "ipv6_instance"
   }
 }
